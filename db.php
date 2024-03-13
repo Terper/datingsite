dc04b901b5cdd25c9ba1f9d6846be72224d16149
Jann Tötterman <jann.totterman@gmail.com>
2024-03-13 20:07:06 +0200

    db connection

--- db.php
+++ db.php
@@ -4,8 +4,8 @@ $env = "dev";
 $db;
 
 if ($env === "dev") {
-  $db = mysqli_connect("localhost", "root", "");
+  $db = mysqli_connect("localhost", "root", "", "datingsite");
 } else {
-  $db = mysqli_connect("localhost", "root", "");
+  $db = mysqli_connect("localhost", "", "", "");
 }
 
